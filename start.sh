f2bf39ac60c380ed71a31ac86d467598270b0665
benoitguigal <benoit.guigal@gmail.com>
2017-05-09 14:29:37 +0200

    disable SSL

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 
 echo "Installing Xerox Printer Phaser 7100D"
 
-lpadmin \
-    -p 'Xerox_Phaser_7100N' \
-    -v 'usb://Xerox/Phaser%207100N?serial=026552' \
-    -P '/usr/share/ppd/Xerox_Phaser_7100N.ppd' \
-    -E
+#lpadmin \
+#    -p 'Xerox_Phaser_7100N' \
+#    -v 'usb://Xerox/Phaser%207100N?serial=026552' \
+#    -P '/usr/share/ppd/Xerox_Phaser_7100N.ppd' \
+#    -E
 
 while true; do sleep 1; done
