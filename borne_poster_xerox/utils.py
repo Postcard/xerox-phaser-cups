d503d85ea64ebcd8c2f461e8bab0ecb85ccd365e
benoitguigal <benoit.guigal@gmail.com>
2017-05-09 12:21:45 +0200

    fix layout

--- borne_poster_xerox/utils.py
+++ borne_poster_xerox/utils.py
@@ -17,21 +17,54 @@ def create_poster(portrait):
         with Image(width=A3_PIXELS_WIDTH, height=A3_PIXELS_HEIGHT, background=bg) as poster:
             poster.units = 'pixelsperinch'
             poster.resolution = 300
-            response = urlopen(portrait['picture_with_caption_1280'])
+            response = urlopen(portrait['picture_1280'])
             with Image(file=response) as picture:
-                margin_top = 413
-                margin_left = 100
-                picture_size = A3_PIXELS_WIDTH - 2 * margin_left
+                margin_top_bottom = 413
+                margin_left_picture = 100
+                picture_size = A3_PIXELS_WIDTH - 2 * margin_left_picture
                 picture.resize(picture_size, picture_size)
-                poster.composite(picture, left=margin_left, top=margin_top)
+                poster.composite(picture, left=margin_left_picture, top=margin_top_bottom)
                 with Drawing() as draw:
-                    draw.font = 'XSWNEBMaisonMonoL'
+                    draw.font = 'MaisonMonoL'
+                    margin_text = 319
+                    # draw caption
+                    draw.font_size = 104
+                    draw.fill_color = Color('white')
+
+                    caption_x = margin_text
+                    caption_y = margin_top_bottom + picture_size - margin_text + margin_left_picture
+                    caption_line_height = 167
+                    offset = 0
+
+                    def draw_with_shadow(x, y, text):
+                        draw.fill_color = Color('black')
+                        for i in range(-2, 3):
+                            for j in range(-2, 3):
+                                if not i == 0 and j == 0:
+                                    draw.text(x + i, y + i, text)
+                        draw.fill_color = Color('white')
+                        draw.text(x, y, text)
+
+                    draw_with_shadow(caption_x, caption_y, portrait['taken_str'])
+
+                    place = portrait['place']
+                    if place:
+                        offset += caption_line_height
+                        draw_with_shadow(caption_x, caption_y - offset, place['name'])
+
+                    event = portrait['event']
+                    if event:
+                        offset += caption_line_height
+                        draw_with_shadow(caption_x, caption_y - offset, event['name'])
+
+                    draw.fill_color = Color('black')
                     draw.font_size = 50
-                    margin_text = 413
                     line_spacing = 83
-                    draw.text(margin_text, A3_PIXELS_HEIGHT - margin_text, WEBSITE)
+                    draw.text(margin_text, A3_PIXELS_HEIGHT - margin_top_bottom, WEBSITE)
                     ticket_number = TICKET_NUMBER % portrait['code']
-                    draw.text(margin_text, A3_PIXELS_HEIGHT - margin_text - line_spacing, ticket_number)
+                    draw.text(margin_text, A3_PIXELS_HEIGHT - margin_top_bottom - line_spacing, ticket_number)
+
+
                     draw(poster)
                     response.close()
                     poster_buffer = io.BytesIO()
