1bf3b966a2f5ccd496536e74f1edad340c8a5263
benoitguigal <benoit.guigal@gmail.com>
2017-05-09 13:45:18 +0200

    change printer options

--- start.sh
+++ start.sh
@@ -6,6 +6,9 @@ lpadmin \
     -p 'Xerox_Phaser_7100N' \
     -v 'usb://Xerox/Phaser%207100N?serial=026552' \
     -P '/usr/share/ppd/Xerox_Phaser_7100N.ppd' \
+    -o 'PageSize=A3' \
+    -o 'StpQuality=High' \
+    -o 'Duplex=DuplexNoTumble' \
     -E
 
 while true; do sleep 1; done
