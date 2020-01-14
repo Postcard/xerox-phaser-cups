5897e3b0ab4c132cf820085c400275aa0e396d5b
myname <jblazy93@gmai.com>
2020-01-14 18:45:31 +0100

    adding xorg and arduino code for makey_makey board

--- start.sh
+++ start.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+# XORG SECTION
+
+
+export DISPLAY=:0.0
+export DBUS_SYSTEM_BUS_ADDRESS=unix:path=/host/run/dbus/system_bus_socket
+
+# rotate screen if env variable is set [normal, inverted, left or right]
+if [[ ! -z "$ROTATE_DISPLAY" ]]; then
+  echo "YES"
+  (sleep 3 && DISPLAY=:0 xrandr -o $ROTATE_DISPLAY) & 
+fi
+
+# start desktop manager
+echo "STARTING X"
+startx
+
+# uncomment to start x without mouse cursor
+# startx -- -nocursor
+
+# uncomment to open an application instead of the desktop
+# startx xterm  
+
+# END OF XORG SECTION
+
 # Start Wifi Access Point if WIFI_ON
 if [ "$WIFI_ON" = 1 ]; then
     /etc/init.d/cups stop
